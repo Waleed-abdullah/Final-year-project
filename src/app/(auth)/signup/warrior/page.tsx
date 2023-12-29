0f33b6de464017b01590e3bc0a60086355ef606f
Waleed Abdullah <68333922+Waleed-abdullah@users.noreply.github.com>
2023-12-29 10:56:06 +0500

    Implement login pages (#31)
    * implemented registerationa and signin pages
    
    * fixed path error
    
    * added the correct images
    
    * deleted unnecessary images
    
    * fixed some responsiveness issues
    
    * added functionality for buttons
    
    * completed complete profile pages
    
    * fixed typo
    
    * fixed small issue with margin
    
    * updated env sample
    
    * fixed api route name
    
    * fixed routing and added error messages and other minor fixes
    
    * added form icons
    
    * fixed routing for user

---
+++ src/app/(auth)/signup/warrior/page.tsx
@@ -0,0 +1,183 @@
+'use client';
+import React, { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { UserType } from '@/src/types/auth/user';
+import WazaLogo from '@/assets/wazaLogos/Wazalogo_Black.svg';
+import trainingImage from '@/assets/signUpPage/warrior/hero-image_1.png';
+import mailIcon from '@/assets/formIcons/mail.svg';
+import lockIcon from '@/assets/formIcons/lock.svg';
+import userIcon from '@/assets/formIcons/user.svg';
+import googleIcon from '@/assets/formIcons/GoogleIcon.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface SignUpData {
+  username: string;
+  email: string;
+  password: string;
+  user_type: UserType;
+  provider: string;
+  is_verified: boolean;
+  profile_pic?: string;
+}
+
+const SignUp = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState<SignUpData>({
+    username: '',
+    email: '',
+    password: '',
+    user_type: UserType.WazaWarrior,
+    provider: 'credentials',
+    is_verified: false,
+  });
+  const [error, setError] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const res = await fetch('/api/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.message || 'Something went wrong!');
+      }
+
+      const data = await res.json();
+      router.push('/signin');
+    } catch (err: any) {
+      console.error(err.message || 'An error occurred');
+      setError(err.message);
+    }
+  };
+
+  const handleSignUpWithGoogle = () => {
+    try {
+      signIn('google', { callbackUrl: '/dashboard' });
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className='flex flex-wrap items-center justify-center w-full min-h-screen'>
+      <div className='lg:basis-1/2 px-20 w-full flex flex-col justify-center gap-[20px] text-black font-desktop-text-bold-1'>
+        <div>
+          <Image src={WazaLogo} alt='logo' className='' />
+        </div>
+        <div className=' text-3xl font-semibold mb-2'>Create a New Account</div>
+        {error.length ? (
+          <div className='text-lg text-red-500'>{error}</div>
+        ) : null}
+        <form onSubmit={handleSubmit} className='w-full mx-auto'>
+          <div className='relative mb-5'>
+            <div className='absolute inset-y-0 start-0 flex items-center ps-2.5 pointer-events-none'>
+              <Image src={userIcon} alt='lockIcon' />
+            </div>
+            <input
+              type='text'
+              id='username'
+              onChange={handleChange}
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block  focus:border-yellow-400 w-full p-2 ps-10'
+              placeholder='Username'
+              required
+            />
+          </div>
+          <div className='relative mb-5'>
+            <div className='absolute inset-y-0 start-0 flex items-center ps-2.5 pointer-events-none'>
+              <Image src={mailIcon} alt='lockIcon' />
+            </div>
+            <input
+              type='email'
+              id='email'
+              onChange={handleChange}
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block  focus:border-yellow-400 w-full p-2 ps-10'
+              placeholder='E-mail'
+              required
+            />
+          </div>
+
+          <div className='relative mb-5'>
+            <div className='absolute inset-y-0 start-0 flex items-center ps-2.5 pointer-events-none'>
+              <Image src={lockIcon} alt='lockIcon' />
+            </div>
+            <input
+              type='password'
+              id='password'
+              onChange={handleChange}
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2 ps-10'
+              placeholder='Password'
+              required
+            />
+          </div>
+
+          <button
+            type='submit'
+            className='relative rounded-[100px] shadow-[0px_10px_10px_rgba(0,_0,_0,_0.05)] h-[50px] text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-400 focus:outline-none sm:text-base md:text-lg lg:text-xl text-color-base-black font-desktop-text-bold-1 w-full lg:px-5 sm:px-1 py-2.5 text-center  '
+          >
+            <div className='relative leading-[101%] font-semibold'>
+              Create New Account
+            </div>
+          </button>
+        </form>
+        <div className='my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300'>
+          <p className='mx-4 mb-0 text-center font-light'>Or</p>
+        </div>
+        <button
+          onClick={() => router.push('/signup/trainer')}
+          className='relative rounded-[100px] shadow-[0px_10px_10px_rgba(0,_0,_0,_0.05)] box-border w-full h-[50px] overflow-hidden text-center hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-black py-1.5 px-5 sm:text-base md:text-lg lg:text-xl text-color-base-black font-desktop-text-bold-1 border-[1px] border-solid border-color-grey-grey-200'
+        >
+          <div className='relative leading-[101%] font-semibold'>
+            Sign up as a Trainer
+          </div>
+        </button>
+
+        <button
+          onClick={handleSignUpWithGoogle}
+          className='relative rounded-[100px] shadow-[0px_10px_10px_rgba(0,_0,_0,_0.05)] box-border w-full h-[50px] overflow-hidden shrink-0  hover:bg-gray-200 focus:ring-4 focus:outline-none flex flex-row focus:ring-black justify-center items-center gap-4  py-1.5 px-5  sm:text-base md:text-lg lg:text-xl text-color-base-black font-desktop-text-bold-1 border-[1px] border-solid border-color-grey-grey-200'
+        >
+          <div>
+            <Image src={googleIcon} alt='googleIcon' />
+          </div>
+          <div className='relative leading-[101%] font-semibold'>
+            Sign up with Google
+          </div>
+        </button>
+
+        <div className=' mb-2 relative text-[20px] leading-[101%] text-black text-center font-poppins'>
+          <span>{`Already have an account? `}</span>
+          <Link href={'/signin'} className='font-semibold hover:font-bold'>
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <div className='lg:basis-1/2 flex items-center justify-center h-full relative'>
+        {/* Training Image */}
+        <div className='w-full h-full'>
+          <Image
+            src={trainingImage}
+            alt='trainingImage'
+            className='w-full h-full object-cover'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
