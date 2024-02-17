f40ebc389a2dde335f74c56f9ab94115964ac3d8
Waleed-Abdullah <w26waleed@gmail.com>
2024-02-17 15:08:30 +0500

    fixed prisma issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient | null;
-
-// Function to create the Prisma client if it doesn't exist
-export const getPrismaClient = () => {
-  if (!prisma) {
-    prisma = new PrismaClient();
-  }
-  return prisma;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-export default getPrismaClient();
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
