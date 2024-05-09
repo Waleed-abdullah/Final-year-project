800da0c182e8e8ab8cb6f3ceb71cd3f3d91e96bc
Waleed-Abdullah <w26waleed@gmail.com>
2024-05-09 23:39:54 +0500

    Fix clear notification button

--- src/components/RightDrawer/TrainerItems/notifications/clear-notifications.tsx
+++ src/components/RightDrawer/TrainerItems/notifications/clear-notifications.tsx
@@ -1,7 +1,7 @@
 export const ClearNotificationsBtn = () => {
   return (
-    <button className='w-full h-[50px] p-2 flex gap-2 justify-center items-center bg-transparent hover:bg-yellow-400 text-black hover:text-white border border-gray-300  hover:border-transparent  font-bold py-2 px-4 rounded-full transition-all duration-500'>
-      <div className=' mr-7'>Clear Notifications</div>
+    <button className='w-[200px] h-[50px] text-center p-2 bg-transparent hover:bg-yellow-400 text-black hover:text-white border border-gray-300  hover:border-transparent  font-bold py-2 px-4 rounded-full transition-all duration-500'>
+      <div>Clear All</div>
     </button>
   );
 };
