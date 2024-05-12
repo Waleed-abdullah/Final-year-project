54d84be3dd44f79116198839d0bf4d796a1091b7
moezAhmad <moez.ahmad.dev@gmail.com>
2024-05-13 00:30:42 +0500

    Updated styles of request card

--- src/components/MessageRequests/index.tsx
+++ src/components/MessageRequests/index.tsx
@@ -57,7 +57,10 @@ const MessageRequests: FC<MessageRequestsProps> = ({
       ) : (
         messageRequests.map((request) => {
           return (
-            <div key={request.user_id}>
+            <div
+              key={request.user_id}
+              className='max-w-[266px]  w-full h-full max-h-[334px] rounded-xl bg-white flex flex-col justify-center items-center gap-3 p-2'
+            >
               <Image
                 src={request.profile_pic || 'https://via.placeholder.com/150'}
                 alt={request.username}
@@ -65,21 +68,27 @@ const MessageRequests: FC<MessageRequestsProps> = ({
                 width={50}
                 height={50}
               />
-              <p>{request.name || 'placeholder'}</p>
-              <button
-                onClick={() => {
-                  acceptMessageRequest(request.user_id);
-                }}
-              >
-                Accept
-              </button>
-              <button
-                onClick={() => {
-                  denyMessageRequest(request.user_id);
-                }}
-              >
-                Decline
-              </button>
+              <p className='font-bold text-lg'>
+                {request.name || 'placeholder'}
+              </p>
+              <div className='flex flex-row justify-between w-3/4'>
+                <button
+                  className='flex items-center justify-center rounded-md p-2 font-medium bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 transition-all duration-500 text-center'
+                  onClick={() => {
+                    acceptMessageRequest(request.user_id);
+                  }}
+                >
+                  Accept
+                </button>
+                <button
+                  className='flex items-center justify-center rounded-md p-2 font-medium bg-red-500 hover:bg-red-400 focus:ring-4 focus:ring-red-300 transition-all duration-500 text-center'
+                  onClick={() => {
+                    denyMessageRequest(request.user_id);
+                  }}
+                >
+                  Decline
+                </button>
+              </div>
             </div>
           );
         })
